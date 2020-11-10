98b593864c56fbd8a387b700fa450057b3bc6ae4
JacobRishe <68757343+JacobRishe@users.noreply.github.com>
2020-11-10 09:53:43 -0800

    added javascript for form to submit to email along with html changes
    changed css diplay to relative and added left and top padding

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
-<script>
-function sendMail(str) {
-    var link = "mailto:jacob.rishe@gmail.com" + "&subject=" + escape("This is my subject") + "&body=" + escape(str);
-    location.href = link;
-}
-function submit_comment() {
-var name = document.forms["contact_form"]["Name"].value;
-var Email = document.forms["contact_form"]["Email"].value;
-var Phone = document.forms["contact_form"]["Phone"].value;
-var Location = document.forms["contact_form"]["Location"].value;
-var Comment = document.forms["contact_form"]["Location"].value;
-if((name !="") && (Email !="") && (Phone !="") && (Location !="") && (Comment !="")) {
-   sendMail("HELLO");
-}
-}
-</script>
+
+window.addEventListener("DOMContentLoaded", function() {
+
+   // get the form elements defined in your form HTML above
+
+   var form = document.getElementById("my-form");
+  // var button = document.getElementById("my-form-button");
+   var status = document.getElementById("status");
+
+   // Success and Error functions for after the form is submitted
+
+   function success() {
+     form.reset();
+     status.classList.add('success')
+     status.innerHTML = "Thanks! I will be in touch!";
+   }
+
+  // function error() {
+  //   status.innerHTML = "Oops! There was a problem.";
+//   }
+
+   // handle the form submission event
+
+   form.addEventListener("submit", function(ev) {
+     ev.preventDefault();
+     var data = new FormData(form);
+     ajax(form.method, form.action, data, success, error);
+   });
+ });
+
+ // helper function for sending an AJAX request
+
+ function ajax(method, url, data, success, error) {
+   var xhr = new XMLHttpRequest();
+   xhr.open(method, url);
+   xhr.setRequestHeader("Accept", "application/json");
+   xhr.onreadystatechange = function() {
+     if (xhr.readyState !== XMLHttpRequest.DONE) return;
+     if (xhr.status === 200) {
+       success(xhr.response, xhr.responseType);
+     } else {
+       error(xhr.status, xhr.response, xhr.responseType);
+     }
+   };
+   xhr.send(data);
+ }
